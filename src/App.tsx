f6fd0cadeb14602d1661d95fc3860301d23a29db
mkbeefcake <mkblockchaindev@outlook.com>
2024-10-10 02:11:55 +0800

    Integrated TailwindCSS & Test message

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div>      
+    <div className='flex items-center justify-center h-screen'>
+      <h1 className='text-4xl font-bold text-blue-500'>TailwindCSS is working</h1>
     </div>
   );
 }
